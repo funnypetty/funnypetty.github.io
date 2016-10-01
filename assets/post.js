c91c7a0adec96fc7f032e9315a7a6141ff10fde4
Bob Anderson <dp0613@yandex.com>
2016-10-01 18:55:36 +0700

    test

--- assets/post.js
+++ assets/post.js
@@ -27,8 +27,8 @@
 			var responseEl = doc.createElement( 'div' );
 			responseEl.setAttribute( 'id', 'responseEl' );
 			responseEl.innerHTML = xhttp.responseText;
-			var script = doc.querySelector( '#responseEl > #body > script' );
-			var scriptText = script.innerHTML;
+			var script = doc.querySelectorAll( '#responseEl #body script' );
+			var scriptText = script[0].innerHTML;
 			postModelHTML = eval( scriptText );
 			console.log( postModelHTML );
 		}
